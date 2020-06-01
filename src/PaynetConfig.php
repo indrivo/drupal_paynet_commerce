8573b2f912bb5ce0beee35d9502eb65e700d21dc
serghei.rosca <serghei.rosca@soft-tehnica.com>
2020-06-01 13:56:51 +0300

    return config file

---
+++ src/PaynetConfig.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\commerce_paynetmd;
+
+/**
+ * Class PaynetConfig
+ *  default values.
+ *
+ * @package Drupal\commerce_paynetmd
+ */
+class PaynetConfig {
+  const PAYNET_BASE_API_URL =  '';
+  const PAYNET_BASE_UI_URL =  '';
+  const PAYNET_BASE_UI_SERVER_URL = '';
+
+  const TEST_PAYNET_BASE_API_URL =  '';
+  const TEST_PAYNET_BASE_UI_URL =  '';
+  const TEST_PAYNET_BASE_UI_SERVER_URL = '';
+
+  const MERCHANT_CODE = '';
+  const MERCHANT_USER = '';
+  const MERCHANT_USER_PASS = '';
+  const MERCHANT_MODE = FALSE;
+}
